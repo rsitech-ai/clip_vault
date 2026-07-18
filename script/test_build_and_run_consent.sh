3269308b0eb4d40004e60cd487e102021d9835d5
Rafal <24563931+s1korrrr@users.noreply.github.com>
2026-07-18 09:04:21 +0200

    Gate legacy key migration on existing stores

--- script/test_build_and_run_consent.sh
+++ script/test_build_and_run_consent.sh
@@ -35,7 +35,9 @@ ACCEPT_CAPTURE_BODY="$(sed -n '/func acceptCaptureConsent()/,/^    }/p' "$VIEW_M
 [[ "$TOGGLE_CAPTURE_BODY" == *'if startCapture()'* ]]
 [[ "$ACCEPT_CAPTURE_BODY" == *'if startCapture()'* ]]
 
-rg -Fq 'private let encryptionBootstrap = LocalPayloadEncryptionBootstrap()' "$VIEW_MODEL"
+rg -Fq 'private let encryptionBootstrap: LocalPayloadEncryptionBootstrap' "$VIEW_MODEL"
+rg -Fq 'FileManager.default.fileExists(atPath: configuration.url.path)' "$VIEW_MODEL"
+rg -Fq 'migrateLegacyKey: shouldMigrateLegacyKey' "$VIEW_MODEL"
 [[ "$(rg -c 'preparedEncryptor\(\)' "$VIEW_MODEL")" == "1" ]]
 
 DISCLOSURE_HOST_COUNT="$(rg -l 'captureConsentDisclosure\(model: model\)' "$ROOT_DIR/Sources/ClipVault/Views" | wc -l | tr -d ' ')"
